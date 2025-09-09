39047307d0b0d683fe603a600678b9a2ca7fd158
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-09-09 11:37:20 +0100

    spike looking at paging approaches

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
 
+  implementation("org.springframework.data:spring-data-commons")
+
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.5.0")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.33") {
