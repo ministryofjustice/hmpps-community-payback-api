5f4ce1fe43c9a30729a0002c6338c1d52f4d805a
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-09-10 09:19:52 +0100

    Use WireMock Spring Support
    This commit adds the wiremock spring boot library and uses it in integration tests via the `@EnableWireMock` annotation.
    
    This allows us to simplify the `HmppsAuthMockServer` configuration as we delegate managing the wiremock server during tests to wiremock itself.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/integration/wiremock/HmppsAuthMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/integration/wiremock/HmppsAuthMockServer.kt
@@ -1,59 +1,30 @@
 package uk.gov.justice.digital.hmpps.communitypaybackapi.integration.wiremock
 
-import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.get
 import com.github.tomakehurst.wiremock.client.WireMock.post
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import com.github.tomakehurst.wiremock.http.HttpHeader
 import com.github.tomakehurst.wiremock.http.HttpHeaders
-import org.junit.jupiter.api.extension.AfterAllCallback
-import org.junit.jupiter.api.extension.BeforeAllCallback
-import org.junit.jupiter.api.extension.BeforeEachCallback
-import org.junit.jupiter.api.extension.ExtensionContext
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
-class HmppsAuthApiExtension :
-  BeforeAllCallback,
-  AfterAllCallback,
-  BeforeEachCallback {
-  companion object {
-    @JvmField
-    val hmppsAuth = HmppsAuthMockServer()
-  }
-
-  override fun beforeAll(context: ExtensionContext) {
-    hmppsAuth.start()
-  }
-
-  override fun beforeEach(context: ExtensionContext) {
-    hmppsAuth.resetRequests()
-  }
-
-  override fun afterAll(context: ExtensionContext) {
-    hmppsAuth.stop()
-  }
-}
-
-class HmppsAuthMockServer : WireMockServer(WIREMOCK_PORT) {
-  companion object {
-    private const val WIREMOCK_PORT = 8090
-  }
+object HmppsAuthMockServer {
 
   fun stubGrantToken() {
-    stubFor(
+    WireMock.stubFor(
       post(urlEqualTo("/auth/oauth/token"))
         .willReturn(
           aResponse()
             .withHeaders(HttpHeaders(HttpHeader("Content-Type", "application/json")))
             .withBody(
               """
-                {
-                  "token_type": "bearer",
-                  "access_token": "ABCDE",
-                  "expires_in": ${LocalDateTime.now().plusHours(2).toEpochSecond(ZoneOffset.UTC)}
-                }
+            {
+              "token_type": "bearer",
+              "access_token": "ABCDE",
+              "expires_in": ${LocalDateTime.now().plusHours(2).toEpochSecond(ZoneOffset.UTC)}
+            }
               """.trimIndent(),
             ),
         ),
@@ -61,7 +32,7 @@ class HmppsAuthMockServer : WireMockServer(WIREMOCK_PORT) {
   }
 
   fun stubHealthPing(status: Int) {
-    stubFor(
+    WireMock.stubFor(
       get("/auth/health/ping").willReturn(
         aResponse()
           .withHeader("Content-Type", "application/json")
