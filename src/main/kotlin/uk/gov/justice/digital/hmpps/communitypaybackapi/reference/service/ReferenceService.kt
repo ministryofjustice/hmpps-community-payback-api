b2cdd7d82185377468c6fc6c50df9ab16015f0fc
dhruv.patel <dhruv.patel@justice.gov.uk>
2025-09-19 16:19:40 +0100

    CPB-105: Created ContactOutcomeEntity.kt and contact_outcomes table to use on the references endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/reference/service/ReferenceService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/reference/service/ReferenceService.kt
@@ -2,14 +2,16 @@ package uk.gov.justice.digital.hmpps.communitypaybackapi.reference.service
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.communitypaybackapi.common.client.CommunityPaybackAndDeliusClient
+import uk.gov.justice.digital.hmpps.communitypaybackapi.reference.entity.ContactOutcomeEntityRepository
 
 @Service
 class ReferenceService(
   val communityPaybackAndDeliusClient: CommunityPaybackAndDeliusClient,
+  val contactOutcomeEntityRepository: ContactOutcomeEntityRepository,
 ) {
   fun getProjectTypes() = communityPaybackAndDeliusClient.getProjectTypes().toDto()
 
-  fun getContactOutcomes() = communityPaybackAndDeliusClient.getContactOutcomes().toDto()
+  fun getContactOutcomes() = contactOutcomeEntityRepository.findAll().toDto()
 
   fun getEnforcementActions() = communityPaybackAndDeliusClient.getEnforcementActions().toDto()
 }
