9442a8c316ef593d38361cce8319060ea14d2dd9
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-10-13 12:31:32 +0100

    Enumerate OffenderDto.objectType
    This commit converts the `OffenderDto.objectType` property from a string to an enumeration, making potential values clearer and making the code generated by the UI(s) more developer friendly.
    
    The various open api annotations that are increasingly breaking DRY principles are still required to ensure the discriminator entries are correctly generated in the Open API.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/common/dto/OffenderDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/common/dto/OffenderDto.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.digital.hmpps.communitypaybackapi.common.dto
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonValue
 import io.swagger.v3.oas.annotations.media.DiscriminatorMapping
 import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.communitypaybackapi.common.dto.OffenderDto.OffenderFullDto
@@ -11,6 +13,7 @@ import uk.gov.justice.digital.hmpps.communitypaybackapi.common.dto.OffenderDto.O
 @JsonTypeInfo(
   use = JsonTypeInfo.Id.NAME,
   property = "objectType",
+  include = JsonTypeInfo.As.EXISTING_PROPERTY,
 )
 @JsonSubTypes(
   JsonSubTypes.Type(OffenderNotFoundDto::class, name = "Not_Found"),
@@ -28,19 +31,39 @@ import uk.gov.justice.digital.hmpps.communitypaybackapi.common.dto.OffenderDto.O
 )
 sealed interface OffenderDto {
   val crn: String
+  val objectType: OffenderType
 
   data class OffenderNotFoundDto(
     override val crn: String,
-  ) : OffenderDto
+  ) : OffenderDto {
+    override val objectType = OffenderType.NOT_FOUND
+  }
 
   data class OffenderLimitedDto(
     override val crn: String,
-  ) : OffenderDto
+  ) : OffenderDto {
+    override val objectType = OffenderType.LIMITED
+  }
 
   data class OffenderFullDto(
     override val crn: String,
     val forename: String,
     val surname: String,
     val middleNames: List<String>,
-  ) : OffenderDto
+  ) : OffenderDto {
+    override val objectType = OffenderType.FULL
+  }
+}
+
+enum class OffenderType(@get:JsonValue val value: String) {
+  NOT_FOUND("Not_Found"),
+  LIMITED("Limited"),
+  FULL("Full"),
+  ;
+
+  companion object {
+    @JvmStatic
+    @JsonCreator
+    fun forValue(value: String) = entries.first { it.value == value }
+  }
 }
