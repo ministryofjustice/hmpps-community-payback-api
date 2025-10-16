0005d6afd649b8464e24f718ea323ea31fb1fc7a
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-10-16 12:24:08 +0100

    Move appointment mapper tests
    These should be defined in the `appointment` package as this is where the corresponding mapper is defined

--- src/test/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/unit/appointment/service/AppointmentMappersTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/unit/appointment/service/AppointmentMappersTest.kt
@@ -12,6 +12,12 @@ import uk.gov.justice.digital.hmpps.communitypaybackapi.appointment.entity.Behav
 import uk.gov.justice.digital.hmpps.communitypaybackapi.appointment.entity.WorkQuality
 import uk.gov.justice.digital.hmpps.communitypaybackapi.appointment.service.fromDto
 import uk.gov.justice.digital.hmpps.communitypaybackapi.appointment.service.toDomainEventDetail
+import uk.gov.justice.digital.hmpps.communitypaybackapi.appointment.service.toDto
+import uk.gov.justice.digital.hmpps.communitypaybackapi.common.client.ProjectAppointment
+import uk.gov.justice.digital.hmpps.communitypaybackapi.common.client.ProjectAppointmentBehaviour
+import uk.gov.justice.digital.hmpps.communitypaybackapi.common.client.ProjectAppointmentWorkQuality
+import uk.gov.justice.digital.hmpps.communitypaybackapi.common.dto.OffenderDto
+import uk.gov.justice.digital.hmpps.communitypaybackapi.common.service.OffenderInfoResult
 import uk.gov.justice.digital.hmpps.communitypaybackapi.factory.valid
 import uk.gov.justice.digital.hmpps.communitypaybackapi.reference.entity.ContactOutcomeEntity
 import uk.gov.justice.digital.hmpps.communitypaybackapi.reference.entity.EnforcementActionEntity
@@ -59,6 +65,80 @@ class AppointmentMappersTest {
       assertThat(result.behaviour).isEqualTo(AppointmentBehaviourDto.UNSATISFACTORY)
       assertThat(result.enforcementActionCode).isEqualTo("EA01")
       assertThat(result.respondBy).isEqualTo(LocalDate.of(2025, 1, 2))
+    }
+  }
+
+  @Nested
+  inner class ProjectAppointmentMapper {
+    @Test
+    fun `should map ProjectAppointment to DTO correctly`() {
+      val id = 101L
+      val projectName = "Community Garden Maintenance"
+      val projectCode = "CGM101"
+      val projectTypeName = "MAINTENANCE"
+      val projectTypeCode = "MAINT"
+      val crn = "CRN1"
+      val contactOutcomeId = UUID.fromString("123e4567-e89b-12d3-a456-426614174000")
+      val enforcementActionId = UUID.fromString("123e4567-e89b-12d3-a456-426614174001")
+      val supervisingTeam = "Team Lincoln"
+      val supervisingTeamCode = "TL01"
+      val providerCode = "PC01"
+      val date = LocalDate.of(2025, 9, 1)
+      val startTime = LocalTime.of(9, 0)
+      val endTime = LocalTime.of(17, 0)
+      val penaltyTime = LocalTime.of(0, 0)
+      val supervisorCode = "CRN1"
+      val respondBy = LocalDate.of(2025, 10, 1)
+      val hiVisWorn = true
+      val workedIntensively = false
+      val workQuality = ProjectAppointmentWorkQuality.SATISFACTORY
+      val behaviour = ProjectAppointmentBehaviour.SATISFACTORY
+      val notes = "This is a test note"
+      val projectAppointment = ProjectAppointment(
+        id = id,
+        projectName = projectName,
+        projectCode = projectCode,
+        projectTypeName = projectTypeName,
+        projectTypeCode = projectTypeCode,
+        crn = crn,
+        supervisingTeam = supervisingTeam,
+        supervisingTeamCode = supervisingTeamCode,
+        providerCode = providerCode,
+        date = date,
+        startTime = startTime,
+        endTime = endTime,
+        penaltyTime = penaltyTime,
+        supervisorCode = supervisorCode,
+        contactOutcomeId = contactOutcomeId,
+        enforcementActionId = enforcementActionId,
+        respondBy = respondBy,
+        hiVisWorn = hiVisWorn,
+        workedIntensively = workedIntensively,
+        workQuality = workQuality,
+        behaviour = behaviour,
+        notes = notes,
+      )
+
+      val result = projectAppointment.toDto(OffenderInfoResult.Limited("CRN1"))
+
+      assertThat(result.id).isEqualTo(id)
+      assertThat(result.projectName).isEqualTo(projectName)
+      assertThat(result.projectCode).isEqualTo(projectCode)
+      assertThat(result.date).isEqualTo(date)
+      assertThat(result.supervisingTeam).isEqualTo(supervisingTeam)
+      assertThat(result.supervisingTeamCode).isEqualTo(supervisingTeamCode)
+      assertThat(result.providerCode).isEqualTo(providerCode)
+      assertThat(result.attendanceData?.supervisorOfficerCode).isEqualTo(supervisorCode)
+      assertThat(result.attendanceData?.penaltyTime).isEqualTo(penaltyTime)
+      assertThat(result.attendanceData?.behaviour).isEqualTo(AppointmentBehaviourDto.SATISFACTORY)
+      assertThat(result.attendanceData?.workQuality).isEqualTo(AppointmentWorkQualityDto.SATISFACTORY)
+      assertThat(result.attendanceData?.hiVisWorn).isEqualTo(hiVisWorn)
+      assertThat(result.attendanceData?.contactOutcomeId).isEqualTo(contactOutcomeId)
+      assertThat(result.enforcementData?.enforcementActionId).isEqualTo(enforcementActionId)
+      assertThat(result.enforcementData?.respondBy).isEqualTo(respondBy)
+      assertThat(result.notes).isEqualTo(notes)
+      assertThat(result.offender.crn).isEqualTo(crn)
+      assertThat(result.offender).isInstanceOf(OffenderDto.OffenderLimitedDto::class.java)
     }
   }
 
