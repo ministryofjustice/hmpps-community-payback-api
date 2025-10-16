2ba65347f0b16bb353d219e171f3efd4f74daa81
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-10-16 12:24:08 +0100

    Move appointment mapper tests
    These should be defined in the `appointment` package as this is where the corresponding mapper is defined

--- src/test/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/unit/project/service/ProjectMappersTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/unit/project/service/ProjectMappersTest.kt
@@ -3,23 +3,16 @@ package uk.gov.justice.digital.hmpps.communitypaybackapi.unit.project.service
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import uk.gov.justice.digital.hmpps.communitypaybackapi.appointment.dto.AppointmentBehaviourDto
-import uk.gov.justice.digital.hmpps.communitypaybackapi.appointment.dto.AppointmentWorkQualityDto
 import uk.gov.justice.digital.hmpps.communitypaybackapi.appointment.service.toDto
-import uk.gov.justice.digital.hmpps.communitypaybackapi.common.client.ProjectAppointment
-import uk.gov.justice.digital.hmpps.communitypaybackapi.common.client.ProjectAppointmentBehaviour
 import uk.gov.justice.digital.hmpps.communitypaybackapi.common.client.ProjectAppointmentSummary
-import uk.gov.justice.digital.hmpps.communitypaybackapi.common.client.ProjectAppointmentWorkQuality
 import uk.gov.justice.digital.hmpps.communitypaybackapi.common.client.ProjectSession
 import uk.gov.justice.digital.hmpps.communitypaybackapi.common.client.ProjectSessionSummaries
 import uk.gov.justice.digital.hmpps.communitypaybackapi.common.client.ProjectSummary
-import uk.gov.justice.digital.hmpps.communitypaybackapi.common.dto.OffenderDto
 import uk.gov.justice.digital.hmpps.communitypaybackapi.common.service.OffenderInfoResult
 import uk.gov.justice.digital.hmpps.communitypaybackapi.project.dto.SessionSummaryDto
 import uk.gov.justice.digital.hmpps.communitypaybackapi.project.service.toDto
 import java.time.LocalDate
 import java.time.LocalTime
-import java.util.UUID
 
 class ProjectMappersTest {
 
@@ -159,80 +152,6 @@ class ProjectMappersTest {
       assertThat(result.appointmentSummaries[1].requirementMinutes).isEqualTo(20)
       assertThat(result.appointmentSummaries[1].completedMinutes).isEqualTo(10)
       assertThat(result.appointmentSummaries[1].offender).isNotNull
-    }
-  }
-
-  @Nested
-  inner class ProjectAppointmentMapper {
-    @Test
-    fun `should map ProjectAppointment to DTO correctly`() {
-      val id = 101L
-      val projectName = "Community Garden Maintenance"
-      val projectCode = "CGM101"
-      val projectTypeName = "MAINTENANCE"
-      val projectTypeCode = "MAINT"
-      val crn = "CRN1"
-      val contactOutcomeId = UUID.fromString("123e4567-e89b-12d3-a456-426614174000")
-      val enforcementActionId = UUID.fromString("123e4567-e89b-12d3-a456-426614174001")
-      val supervisingTeam = "Team Lincoln"
-      val supervisingTeamCode = "TL01"
-      val providerCode = "PC01"
-      val date = LocalDate.of(2025, 9, 1)
-      val startTime = LocalTime.of(9, 0)
-      val endTime = LocalTime.of(17, 0)
-      val penaltyTime = LocalTime.of(0, 0)
-      val supervisorCode = "CRN1"
-      val respondBy = LocalDate.of(2025, 10, 1)
-      val hiVisWorn = true
-      val workedIntensively = false
-      val workQuality = ProjectAppointmentWorkQuality.SATISFACTORY
-      val behaviour = ProjectAppointmentBehaviour.SATISFACTORY
-      val notes = "This is a test note"
-      val projectAppointment = ProjectAppointment(
-        id = id,
-        projectName = projectName,
-        projectCode = projectCode,
-        projectTypeName = projectTypeName,
-        projectTypeCode = projectTypeCode,
-        crn = crn,
-        supervisingTeam = supervisingTeam,
-        supervisingTeamCode = supervisingTeamCode,
-        providerCode = providerCode,
-        date = date,
-        startTime = startTime,
-        endTime = endTime,
-        penaltyTime = penaltyTime,
-        supervisorCode = supervisorCode,
-        contactOutcomeId = contactOutcomeId,
-        enforcementActionId = enforcementActionId,
-        respondBy = respondBy,
-        hiVisWorn = hiVisWorn,
-        workedIntensively = workedIntensively,
-        workQuality = workQuality,
-        behaviour = behaviour,
-        notes = notes,
-      )
-
-      val result = projectAppointment.toDto(OffenderInfoResult.Limited("CRN1"))
-
-      assertThat(result.id).isEqualTo(id)
-      assertThat(result.projectName).isEqualTo(projectName)
-      assertThat(result.projectCode).isEqualTo(projectCode)
-      assertThat(result.date).isEqualTo(date)
-      assertThat(result.supervisingTeam).isEqualTo(supervisingTeam)
-      assertThat(result.supervisingTeamCode).isEqualTo(supervisingTeamCode)
-      assertThat(result.providerCode).isEqualTo(providerCode)
-      assertThat(result.attendanceData?.supervisorOfficerCode).isEqualTo(supervisorCode)
-      assertThat(result.attendanceData?.penaltyTime).isEqualTo(penaltyTime)
-      assertThat(result.attendanceData?.behaviour).isEqualTo(AppointmentBehaviourDto.SATISFACTORY)
-      assertThat(result.attendanceData?.workQuality).isEqualTo(AppointmentWorkQualityDto.SATISFACTORY)
-      assertThat(result.attendanceData?.hiVisWorn).isEqualTo(hiVisWorn)
-      assertThat(result.attendanceData?.contactOutcomeId).isEqualTo(contactOutcomeId)
-      assertThat(result.enforcementData?.enforcementActionId).isEqualTo(enforcementActionId)
-      assertThat(result.enforcementData?.respondBy).isEqualTo(respondBy)
-      assertThat(result.notes).isEqualTo(notes)
-      assertThat(result.offender.crn).isEqualTo(crn)
-      assertThat(result.offender).isInstanceOf(OffenderDto.OffenderLimitedDto::class.java)
     }
   }
 }
