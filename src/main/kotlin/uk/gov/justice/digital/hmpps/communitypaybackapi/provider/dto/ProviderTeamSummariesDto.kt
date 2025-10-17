ae0e3b3a64e018bc2552d30c002aa92f94452d78
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-10-17 14:23:06 +0100

    Remove ID from teams summary response
    This commit also deprecates the ID on the corresponding UI facing API.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/provider/dto/ProviderTeamSummariesDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/provider/dto/ProviderTeamSummariesDto.kt
@@ -3,12 +3,13 @@ package uk.gov.justice.digital.hmpps.communitypaybackapi.provider.dto
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class ProviderTeamSummaryDto(
-  @param:Schema(description = "Community Payback (UPW) provider team id", example = "1001")
-  val id: Long,
   @param:Schema(description = "Community Payback (UPW) provider team code", example = "ABD123")
   val code: String,
   @param:Schema(description = "Community Payback (UPW) provider team name", example = "Team Lincoln")
   val name: String,
+  @Deprecated("Id will be removed")
+  @param:Schema(description = "Community Payback (UPW) provider team id", example = "1001", deprecated = true)
+  val id: Long = 0,
 )
 
 data class ProviderTeamSummariesDto(
