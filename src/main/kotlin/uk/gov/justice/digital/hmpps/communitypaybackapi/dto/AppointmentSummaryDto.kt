6db6c03548e4f711bd147e00947250ae6daa4a4b
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-10-23 10:41:25 +0100

    Move `AppointmentSummaryDto` into standalone class

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/dto/AppointmentSummaryDto.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.communitypaybackapi.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class AppointmentSummaryDto(
+  val id: Long,
+  @param:Schema(
+    description = "How many community payback minutes the offender is required to complete",
+    example = "2400",
+  )
+  val requirementMinutes: Int,
+  @param:Schema(description = "How many community payback minutes the offender has completed to date", example = "480")
+  val completedMinutes: Int,
+  val offender: OffenderDto,
+)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/dto/SessionDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/dto/SessionDto.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.communitypaybackapi.dto
 
-import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 import java.time.LocalTime
 
@@ -12,16 +11,4 @@ data class SessionDto(
   val startTime: LocalTime,
   val endTime: LocalTime,
   val appointmentSummaries: List<AppointmentSummaryDto>,
-)
-
-data class AppointmentSummaryDto(
-  val id: Long,
-  @param:Schema(
-    description = "How many community payback minutes the offender is required to complete",
-    example = "2400",
-  )
-  val requirementMinutes: Int,
-  @param:Schema(description = "How many community payback minutes the offender has completed to date", example = "480")
-  val completedMinutes: Int,
-  val offender: OffenderDto,
 )
