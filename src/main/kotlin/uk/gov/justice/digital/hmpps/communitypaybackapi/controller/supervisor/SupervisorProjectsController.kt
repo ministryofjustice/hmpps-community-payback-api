f5115016d9d220249aef19bd231d004ce1e8fe31
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-10-23 10:33:20 +0100

    Add supervisor GET session endpoint
    This is identical to the admin GET session endpoint

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/controller/supervisor/SupervisorProjectsController.kt
@@ -0,0 +1,57 @@
+package uk.gov.justice.digital.hmpps.communitypaybackapi.controller.supervisor
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import uk.gov.justice.digital.hmpps.communitypaybackapi.service.ProjectService
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+import java.time.LocalDate
+import java.time.LocalTime
+
+@SupervisorUiController
+class SupervisorProjectsController(
+  val projectService: ProjectService,
+) {
+
+  @GetMapping(
+    path = [ "/supervisor/projects/{projectCode}/sessions/{date}"],
+    produces = [MediaType.APPLICATION_JSON_VALUE],
+  )
+  @Operation(
+    description = "Get project allocations within date range for a specific team",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successful project allocations response",
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Bad request - invalid date format or parameters",
+        content = [
+          Content(
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+    ],
+  )
+  fun getSession(
+    @PathVariable projectCode: String,
+    @Parameter(description = "Date", example = "2025-01-01")
+    @PathVariable
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) date: LocalDate,
+    @Parameter(description = "Start time", example = "09:00")
+    @RequestParam
+    @DateTimeFormat(pattern = "HH:mm") startTime: LocalTime,
+    @Parameter(description = "End time", example = "17:00")
+    @RequestParam
+    @DateTimeFormat(pattern = "HH:mm") endTime: LocalTime,
+  ) = projectService.getSession(projectCode, date, startTime, endTime)
+}
