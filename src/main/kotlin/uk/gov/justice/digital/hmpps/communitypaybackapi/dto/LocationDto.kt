a5d6268646dce5455d457773850aa822b573dfdf
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-10-29 12:17:35 +0000

    Extract LocationDto to standalone file

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/dto/LocationDto.kt
@@ -0,0 +1,10 @@
+package uk.gov.justice.digital.hmpps.communitypaybackapi.dto
+
+data class LocationDto(
+  val buildingName: String?,
+  val buildingNumber: String?,
+  val streetName: String?,
+  val townCity: String?,
+  val county: String?,
+  val postCode: String?,
+)
