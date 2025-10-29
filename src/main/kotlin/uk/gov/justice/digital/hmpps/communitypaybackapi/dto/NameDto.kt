b91efc52ce9458bc75302e10ccaf27ff4c1893d8
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-10-29 16:43:14 +0000

    Add structured Name & Grade to Supervisor DTO
    These should be used instead of the now deprecated `fullName`.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/dto/NameDto.kt
@@ -0,0 +1,7 @@
+package uk.gov.justice.digital.hmpps.communitypaybackapi.dto
+
+data class NameDto(
+  val forename: String,
+  val surname: String,
+  val middleNames: List<String> = emptyList(),
+)
