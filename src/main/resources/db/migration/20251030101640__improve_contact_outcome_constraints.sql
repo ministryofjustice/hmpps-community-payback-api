9b9403f34eee6cccab4b9e27f773b8b4b9752470
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-10-30 10:35:25 +0000

    Improve constraints on `contact_outcomes` table
    This commit adds constraints to the table that reflect how the entity is defined in code wrt. nullability

---
+++ src/main/resources/db/migration/20251030101640__improve_contact_outcome_constraints.sql
@@ -0,0 +1,3 @@
+ALTER TABLE contact_outcomes ALTER COLUMN enforceable SET NOT NULL;
+ALTER TABLE contact_outcomes ALTER COLUMN created_at SET NOT NULL;
+ALTER TABLE contact_outcomes ALTER COLUMN updated_at SET NOT NULL;
