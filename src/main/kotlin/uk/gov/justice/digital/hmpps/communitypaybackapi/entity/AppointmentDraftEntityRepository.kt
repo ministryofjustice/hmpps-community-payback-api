f7251abb62251c574016f767a5951d6c97bed493
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-10-31 10:56:36 +0000

    Remove unused `AppointmentDraftEntity`
    There is no corresponding table defined, so no table needs removing

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/entity/AppointmentDraftEntityRepository.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.communitypaybackapi.entity
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import java.util.UUID
-
-@Repository
-interface AppointmentDraftEntityRepository : JpaRepository<AppointmentDraftEntity, UUID>
