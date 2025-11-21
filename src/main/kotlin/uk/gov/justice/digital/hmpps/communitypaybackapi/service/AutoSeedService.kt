e0f5c2c37e864d5f491f0ca49c49980103040d51
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-11-21 11:37:27 +0000

    Auto allocate session in test environments
    This commit adds an AutoSeedService that is enabled in dev and test environments. This will ensure a supervisor allocation exists for our test user against a future session on project `N56123456`, which is configured to be returned by wiremock

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/service/AutoSeedService.kt
@@ -0,0 +1,41 @@
+package uk.gov.justice.digital.hmpps.communitypaybackapi.service
+
+import jakarta.annotation.PostConstruct
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.communitypaybackapi.dto.SessionIdDto
+import java.time.LocalDate
+
+@Service
+@ConditionalOnProperty(name = ["community-payback.auto-seed.enabled"], havingValue = "true")
+class AutoSeedService(
+  val environmentService: EnvironmentService,
+  val sessionService: SessionService,
+) {
+
+  private companion object {
+    const val SUPERVISOR_SESSION_PROJECT_CODE = "N56123456"
+    val SUPERVISOR_SESSION_PROJECT_DAY: LocalDate = LocalDate.of(2026, 3, 1)
+    const val SUPERVISOR_CODE = "N56A108"
+  }
+
+  @PostConstruct
+  fun onStartup() {
+    if (environmentService.isNotATestEnvironment()) {
+      error("auto seed should not be enabled outside of test environments")
+    }
+
+    ensureSupervisorAllocationExists()
+  }
+
+  private fun ensureSupervisorAllocationExists() {
+    sessionService.allocateSupervisor(
+      sessionId = SessionIdDto(
+        projectCode = SUPERVISOR_SESSION_PROJECT_CODE,
+        day = SUPERVISOR_SESSION_PROJECT_DAY,
+      ),
+      supervisorCode = SUPERVISOR_CODE,
+      allocatedByUsername = "AutoSeedService",
+    )
+  }
+}
