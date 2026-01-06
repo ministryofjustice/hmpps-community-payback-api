d4a82ce49b834ecb4defca28c4cafd8434cf5433
Toby Batch <toby.batch@digital.justice.gov.uk>
2026-01-06 17:28:51 +0000

    chore(CPB-481): update springboot and other deps/plugins

--- src/test/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/integration/util/WebTestExtensions.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/integration/util/WebTestExtensions.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.communitypaybackapi.integration.util
 
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.returnResult
 
-inline fun <reified T> WebTestClient.ResponseSpec.bodyAsObject(): T = this.returnResult(T::class.java).responseBody.blockFirst()!!
+inline fun <reified T : Any> WebTestClient.ResponseSpec.bodyAsObject(): T = this.returnResult<T>().responseBody.blockFirst()!!
