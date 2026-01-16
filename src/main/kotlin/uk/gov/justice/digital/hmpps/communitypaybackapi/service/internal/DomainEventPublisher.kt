13e8da0b6d1c72c48476cd3de62ec65906588107
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-01-16 10:56:59 +0000

    Add optional domain event environment attribute

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/service/internal/DomainEventPublisher.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/service/internal/DomainEventPublisher.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.digital.hmpps.communitypaybackapi.service.internal
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import software.amazon.awssdk.services.sns.model.MessageAttributeValue
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.publish
 import java.time.OffsetDateTime
@@ -10,6 +12,14 @@ import java.time.OffsetDateTime
 class DomainEventPublisher(
   private val hmppsQueueService: HmppsQueueService,
   private val objectMapper: ObjectMapper,
+  /**
+   * Set as a message attribute which can then be used as filter in the
+   * domain event consumer queue configuration. This is only used in
+   * the dev and test environments which share the same underlying
+   * domain event queue
+   */
+  @param:Value("\${community-payback.domain-events.environment:null}")
+  private val environment: String?,
 ) {
   private val domainEventsTopic by lazy {
     hmppsQueueService.findByTopicId("hmppseventtopic") ?: error("hmppseventtopic not found")
@@ -19,6 +29,9 @@ class DomainEventPublisher(
     domainEventsTopic.publish(
       eventType = domainEvent.eventType,
       event = objectMapper.writeValueAsString(domainEvent),
+      attributes = environment?.let {
+        mapOf("environment" to MessageAttributeValue.builder().dataType("String").stringValue(environment).build())
+      } ?: emptyMap(),
     )
   }
 }
