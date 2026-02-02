23fa28f35654ed78a16be94676d1e7a63d26489c
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-02-02 09:51:45 +0000

    Remove automatic supervisor allocation on startup
    Removing defunct supervisor allocation code from auto seeding. This was added when the supervisor ui relied on sessions being allocated to a specific user, which is no longer the case. Furthermore, it relied on wiremocked project codes, which are no longer used.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/service/AutoSeedService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/service/AutoSeedService.kt
@@ -3,21 +3,12 @@ package uk.gov.justice.digital.hmpps.communitypaybackapi.service
 import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.communitypaybackapi.dto.SessionIdDto
-import java.time.LocalDate
 
 @Service
 @ConditionalOnProperty(name = ["community-payback.auto-seed.enabled"], havingValue = "true")
 class AutoSeedService(
   val environmentService: EnvironmentService,
-  val sessionService: SessionService,
 ) {
-
-  private companion object {
-    const val SUPERVISOR_SESSION_PROJECT_CODE = "N56123456"
-    val SUPERVISOR_SESSION_PROJECT_DAY: LocalDate = LocalDate.of(2026, 3, 1)
-    const val SUPERVISOR_CODE = "N56A108"
-  }
 
   @PostConstruct
   fun onStartup() {
@@ -25,17 +16,6 @@ class AutoSeedService(
       error("auto seed should not be enabled outside of test environments")
     }
 
-    ensureSupervisorAllocationExists()
-  }
-
-  private fun ensureSupervisorAllocationExists() {
-    sessionService.allocateSupervisor(
-      sessionId = SessionIdDto(
-        projectCode = SUPERVISOR_SESSION_PROJECT_CODE,
-        day = SUPERVISOR_SESSION_PROJECT_DAY,
-      ),
-      supervisorCode = SUPERVISOR_CODE,
-      allocatedByUsername = "AutoSeedService",
-    )
+    // currently autoseeding does nothing.
   }
 }
