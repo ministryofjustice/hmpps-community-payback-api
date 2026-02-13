f12e518f0e2cbfae8d22eda7c22d578a37debb9a
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-02-13 11:10:24 +0000

    Add unit tests for project mappers

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/service/mappers/ProjectMappers.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/service/mappers/ProjectMappers.kt
@@ -1,7 +1,32 @@
 package uk.gov.justice.digital.hmpps.communitypaybackapi.service.mappers
 
+import uk.gov.justice.digital.hmpps.communitypaybackapi.client.NDBeneficiaryDetails
+import uk.gov.justice.digital.hmpps.communitypaybackapi.client.NDProject
 import uk.gov.justice.digital.hmpps.communitypaybackapi.client.NDProjectOutcomeSummary
+import uk.gov.justice.digital.hmpps.communitypaybackapi.dto.BeneficiaryDetailsDto
+import uk.gov.justice.digital.hmpps.communitypaybackapi.dto.ProjectDto
 import uk.gov.justice.digital.hmpps.communitypaybackapi.dto.ProjectOutcomeSummaryDto
+import uk.gov.justice.digital.hmpps.communitypaybackapi.entity.ProjectTypeEntity
+
+fun NDProject.toDto(
+  projectType: ProjectTypeEntity,
+) = ProjectDto(
+  projectName = this.name,
+  projectCode = this.code,
+  projectType = projectType.toDto(),
+  location = this.location.toDto(),
+  hiVisRequired = this.hiVisRequired,
+  beneficiaryDetails = this.beneficiaryDetails.toDto(),
+)
+
+fun NDBeneficiaryDetails.toDto() = BeneficiaryDetailsDto(
+  beneficiary = this.name,
+  contactName = this.contactName,
+  emailAddress = this.emailAddress,
+  website = this.website,
+  telephoneNumber = this.telephoneNumber,
+  location = location.toDto(),
+)
 
 fun NDProjectOutcomeSummary.toDto() = ProjectOutcomeSummaryDto(
   projectName = this.name,
