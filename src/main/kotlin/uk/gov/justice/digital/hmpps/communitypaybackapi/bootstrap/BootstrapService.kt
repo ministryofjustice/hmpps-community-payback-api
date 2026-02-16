d4944eadee14280d37f8e92c077a447d18001fc6
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-02-16 14:05:04 +0000

    Acquire lock for autoseeding
    When bootstrapping the service acquire a distrubted lock when running autoseeders.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/bootstrap/BootstrapService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/bootstrap/BootstrapService.kt
@@ -5,22 +5,31 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.communitypaybackapi.service.EnvironmentService
+import uk.gov.justice.digital.hmpps.communitypaybackapi.service.internal.LockService
+import java.time.Duration
 
 @Service
 @ConditionalOnProperty(name = ["community-payback.bootstrap.enabled"], havingValue = "true")
 class BootstrapService(
   private val environmentService: EnvironmentService,
   private val seeders: List<AutoSeeder> = listOf(),
+  private val lockService: LockService,
 ) {
   private val log = LoggerFactory.getLogger(this::class.java)
 
-  @Suppress("TooGenericExceptionCaught")
   @PostConstruct
   fun onStartup() {
     if (environmentService.isNotATestEnvironment()) {
       error("boot strap should not be enabled outside of test environments")
     }
 
+    lockService.withDistributedLock(key = "bootstrap", leaseTime = Duration.ofSeconds(10)) {
+      runAutoSeeders()
+    }
+  }
+
+  @Suppress("TooGenericExceptionCaught")
+  private fun runAutoSeeders() {
     if (seeders.isEmpty()) {
       log.info("No auto-seeders registered")
     } else {
