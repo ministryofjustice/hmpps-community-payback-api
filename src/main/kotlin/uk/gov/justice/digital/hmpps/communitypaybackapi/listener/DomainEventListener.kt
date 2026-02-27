8009080ccd3dcf99eb8fddb081bb6fac6ee25b88
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-02-27 09:42:34 +0000

    Remove appointment scheduling enabled config
    This is no longer required as scheduling is stable.
    
    I’m removing this now because we’re going to add scheduled jobs and the config for that will be very similarly named, so it was a good opportunity to tidy this up.
    
    If scheduling does need to be disabled, the`community-payback.appointment-scheduling.dryRun` option is still available.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/listener/DomainEventListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/listener/DomainEventListener.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import io.awspring.cloud.sqs.annotation.SqsListener
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.messaging.MessageHeaders
 import org.springframework.messaging.handler.annotation.Headers
 import org.springframework.stereotype.Service
@@ -23,7 +22,6 @@ import java.util.UUID
  * See https://tech-docs.hmpps.service.justice.gov.uk/common-kotlin-patterns/domain-events/
  */
 @Service
-@ConditionalOnProperty(name = ["community-payback.scheduling.enabled"], havingValue = "true")
 class DomainEventListener(
   private val jsonMapper: JsonMapper,
   private val sqsListenerErrorHandler: SqsListenerErrorHandler,
