822c9736bc731757402673a886e1f8922044d0fb
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-03-30 17:08:13 +0100

    Improve local API request/response logging
    This commit improves the request/response logging filter to log out the information on a single more readable line e.g.
    
    ```
    2026-03-31 09:13:42.897  INFO 7162 --- [nio-8080-exec-9] u.g.j.d.h.c.config.ApiLoggingFilter      :
    
    Request To: GET /admin/providers/N56/teams
    Request Body:
    Response Code: 200
    Response Body: {"providers":[{"code":"N56UAT","name":"Unallocated Team(N56)"},{"code":"N56DTX","name":"Default Designated Transfer Team"},{"code":"N56CPM","name":"CPB Manual Test Team"},{"code":"N56CPB","name":"CPB Automated Test Team"}]}
    
     |
    ```
    
    This filter continues to only be applied in local deploys.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/config/RequestLoggingFilter.kt
@@ -0,0 +1,114 @@
+package uk.gov.justice.digital.hmpps.communitypaybackapi.config
+
+import jakarta.annotation.PostConstruct
+import jakarta.servlet.AsyncEvent
+import jakarta.servlet.AsyncListener
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import org.springframework.web.util.ContentCachingRequestWrapper
+import org.springframework.web.util.ContentCachingResponseWrapper
+import uk.gov.justice.digital.hmpps.communitypaybackapi.service.EnvironmentService
+import java.io.IOException
+
+@Component
+@ConditionalOnProperty(name = ["community-payback.request-logging-enabled"], havingValue = "true")
+class RequestLoggingFilter(
+  val environmentService: EnvironmentService,
+) : OncePerRequestFilter() {
+
+  @PostConstruct
+  fun checkEnvironment() {
+    if (environmentService.isNotATestEnvironment()) {
+      error("request logging should not be enabled outside of test environments")
+    }
+  }
+
+  @Throws(ServletException::class, IOException::class)
+  override fun doFilterInternal(
+    request: HttpServletRequest,
+    response: HttpServletResponse,
+    filterChain: FilterChain,
+  ) {
+    if (request.requestURI.startsWith("/health")) {
+      filterChain.doFilter(request, response)
+    } else {
+      applyFilterWithLogging(request, response, filterChain)
+    }
+  }
+
+  private fun applyFilterWithLogging(
+    request: HttpServletRequest,
+    response: HttpServletResponse,
+    filterChain: FilterChain,
+  ) {
+    val wrappedRequest = ContentCachingRequestWrapper(request, 0)
+    val wrappedResponse = ContentCachingResponseWrapper(response)
+
+    filterChain.doFilter(wrappedRequest, wrappedResponse)
+
+    val requestBody = wrappedRequest.requestBodyString()
+    val responseBody = wrappedResponse.responseBodyString()
+
+    val sanitisedQueryString = request.queryString?.let {
+      "?$it"
+    } ?: ""
+
+    logger.info(
+      """
+        
+Request To: ${request.method} ${request.requestURI}$sanitisedQueryString
+Request Body: $requestBody
+Response Code: ${response.status} 
+Response Body: $responseBody
+
+""",
+    )
+
+    writeResponseToCaller(wrappedRequest, wrappedResponse)
+  }
+
+  private fun writeResponseToCaller(
+    request: ContentCachingRequestWrapper,
+    response: ContentCachingResponseWrapper,
+  ) {
+    if (request.isAsyncStarted) {
+      request.asyncContext.addListener(
+        object : DefaultAsyncListener {
+          override fun onComplete(p0: AsyncEvent?) {
+            response.copyBodyToResponse()
+          }
+        },
+      )
+    } else {
+      response.copyBodyToResponse()
+    }
+  }
+
+  private fun ContentCachingRequestWrapper.requestBodyString() = String(contentAsByteArray)
+
+  private fun ContentCachingResponseWrapper.responseBodyString() = if (contentType == "application/json") {
+    String(contentAsByteArray)
+  } else {
+    "Response Body not logged as content type is $contentType"
+  }
+
+  private interface DefaultAsyncListener : AsyncListener {
+
+    override fun onTimeout(p0: AsyncEvent?) {
+      // deliberately empty
+    }
+
+    override fun onError(p0: AsyncEvent?) {
+      // deliberately empty
+    }
+
+    override fun onStartAsync(p0: AsyncEvent?) {
+      // deliberately empty
+    }
+  }
+}
