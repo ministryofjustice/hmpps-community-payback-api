8ead48b30748fef12f56ef74eb63b4d8f39af617
dhruv.patel <dhruv.patel@justice.gov.uk>
2026-04-24 16:43:52 +0100

    CPB-868: Removed comments

--- gatling/src/gatling/kotlin/uk/gov/justice/digital/hmpps/communitypayback/simulations/journeys/CaseAdminOnlineLearningJourneySimulation.kt
+++ gatling/src/gatling/kotlin/uk/gov/justice/digital/hmpps/communitypayback/simulations/journeys/CaseAdminOnlineLearningJourneySimulation.kt
@@ -42,85 +42,74 @@ class CaseAdminOnlineLearningJourneySimulation : BaseSimulationBackEndApi() {
       .registerKotlinModule()
       .registerModule(JavaTimeModule())
 
-    fun buildScenario(): ChainBuilder {
-      return CoreDsl.feed(CoreDsl.csv("data/ete-course-completions-data.csv").circular())
-        .exec { session ->
-          session.set("formId", UUID.randomUUID().toString())
-        }
-        .exec(
-          HttpDsl.http("GET Providers")
-            .get("/admin/providers?username=${sc.caseAdminUsername}")
-            .check(HttpDsl.status().`is`(200)),
+    fun buildScenario(): ChainBuilder = CoreDsl.feed(CoreDsl.csv("data/ete-course-completions-data.csv").circular())
+      .exec { session ->
+        session.set("formId", UUID.randomUUID().toString())
+      }
+      .exec(
+        HttpDsl.http("GET Providers")
+          .get("/admin/providers?username=${sc.caseAdminUsername}")
+          .check(HttpDsl.status().`is`(200)),
+      )
+      .exec(
+        HttpDsl.http("GET Community Campus PDUs")
+          .get("/common/references/community-campus-pdus")
+          .check(HttpDsl.status().`is`(200)),
+      )
+      .exec(
+        HttpDsl.http("GET Course Completions")
+          .get("/admin/providers/#{providerCode}/course-completions?pduId=#{pduId}&resolutionStatus=Unresolved&sort=completionDateTime,asc&page=0&size=10")
+          .check(HttpDsl.status().`is`(200)),
+      )
+      .exec(
+        HttpDsl.http("GET Course Completion Details")
+          .get("/admin/course-completions/#{courseCompletionId}")
+          .check(HttpDsl.status().`is`(200)),
+      )
+      .exec(
+        HttpDsl.http("PUT Form")
+          .put("/common/forms/COURSE_COMPLETION_RESOLUTION/#{formId}")
+          .asJson()
+          .body(CoreDsl.StringBody("""{"crn":"#{crn}"}"""))
+          .check(HttpDsl.status().`is`(200)),
+      )
+      .exec(
+        HttpDsl.http("GET Offender Summary")
+          .get("/admin/offenders/#{crn}/summary")
+          .check(HttpDsl.status().`is`(200)),
+      )
+      .exec { session ->
+        val courseCompletionResolution = CourseCompletionResolutionDto(
+          crn = session.getString("crn"),
+          type = CourseCompletionResolutionTypeDto.CREDIT_TIME,
+          creditTimeDetails = CourseCompletionCreditTimeDetailsDto(
+            deliusEventNumber = 1,
+            date = LocalDate.now(),
+            appointmentIdToUpdate = session.getLong("appointmentId"),
+            minutesToCredit = 180,
+            contactOutcomeCode = "ATTC",
+            projectCode = session.getString("projectCode")!!,
+            notes = null,
+            alertActive = false,
+            sensitive = false,
+          ),
+          dontCreditTimeDetails = null,
         )
-//        .pause(1.seconds.toJavaDuration())
-        .exec(
-          HttpDsl.http("GET Community Campus PDUs")
-            .get("/common/references/community-campus-pdus")
-            .check(HttpDsl.status().`is`(200)),
-        )
-//        .pause(1.seconds.toJavaDuration())
-        .exec(
-          HttpDsl.http("GET Course Completions")
-            .get("/admin/providers/#{providerCode}/course-completions?pduId=#{pduId}&resolutionStatus=Unresolved&sort=completionDateTime,asc&page=0&size=10")
-            .check(HttpDsl.status().`is`(200)),
-        )
-//        .pause(1.seconds.toJavaDuration())
-        .exec(
-          HttpDsl.http("GET Course Completion Details")
-            .get("/admin/course-completions/#{courseCompletionId}")
-            .check(HttpDsl.status().`is`(200)),
-        )
-//        .pause(1.seconds.toJavaDuration())
-        .exec(
-          HttpDsl.http("PUT Form")
-            .put("/common/forms/COURSE_COMPLETION_RESOLUTION/#{formId}")
-            .asJson()
-            .body(CoreDsl.StringBody("""{"crn":"#{crn}"}"""))
-            .check(HttpDsl.status().`is`(200)),
-        )
-//        .pause(1.seconds.toJavaDuration())
-        .exec(
-          HttpDsl.http("GET Offender Summary")
-            .get("/admin/offenders/#{crn}/summary")
-            .check(HttpDsl.status().`is`(200)),
-        )
-//        .pause(1.seconds.toJavaDuration())
-        .exec { session ->
-          val courseCompletionResolution = CourseCompletionResolutionDto(
-            crn = session.getString("crn"),
-            type = CourseCompletionResolutionTypeDto.CREDIT_TIME,
-            creditTimeDetails = CourseCompletionCreditTimeDetailsDto(
-              deliusEventNumber = 1,
-              date = LocalDate.now(),
-              appointmentIdToUpdate = session.getLong("appointmentId"),
-              minutesToCredit = 180,
-              contactOutcomeCode = "ATTC",
-              projectCode = session.getString("projectCode")!!,
-              notes = null,
-              alertActive = false,
-              sensitive = false,
-            ),
-            dontCreditTimeDetails = null,
-          )
-          session.set("courseCompletionResolution", objectMapper.writeValueAsString(courseCompletionResolution))
-        }
-//        .pause(1.seconds.toJavaDuration())
-        .exec(
-          HttpDsl.http("PUT Form")
-            .put("/common/forms/COURSE_COMPLETION_RESOLUTION/#{formId}")
-            .asJson()
-            .body(CoreDsl.StringBody("#{courseCompletionResolution}"))
-            .check(HttpDsl.status().`is`(200)),
-        )
-//        .pause(1.seconds.toJavaDuration())
-        .exec(
-          HttpDsl.http("POST Course Completion Resolution")
-            .post("/admin/course-completions/#{courseCompletionId}/resolution")
-            .asJson()
-            .body(CoreDsl.StringBody("#{courseCompletionResolution}"))
-            .check(HttpDsl.status().`in`(204, 409)),
-        )
-//        .pause(1.seconds.toJavaDuration())
-    }
+        session.set("courseCompletionResolution", objectMapper.writeValueAsString(courseCompletionResolution))
+      }
+      .exec(
+        HttpDsl.http("PUT Form")
+          .put("/common/forms/COURSE_COMPLETION_RESOLUTION/#{formId}")
+          .asJson()
+          .body(CoreDsl.StringBody("#{courseCompletionResolution}"))
+          .check(HttpDsl.status().`is`(200)),
+      )
+      .exec(
+        HttpDsl.http("POST Course Completion Resolution")
+          .post("/admin/course-completions/#{courseCompletionId}/resolution")
+          .asJson()
+          .body(CoreDsl.StringBody("#{courseCompletionResolution}"))
+          .check(HttpDsl.status().`in`(204, 409)),
+      )
   }
 }
