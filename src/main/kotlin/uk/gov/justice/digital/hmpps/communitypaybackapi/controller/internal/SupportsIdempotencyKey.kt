c2e72e46449f40fb153903015af55960625a8c67
Dan Livings <dan.livings@justice.gov.uk>
2026-05-22 10:42:37 +0100

    CPB-816: Add `SupportsIdempotencyKey` annotation
    This formalises the `supportsIdempotencyKey` tag which will be used in multiple
    places as more endpoints become explicitly able to support this feature.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitypaybackapi/controller/internal/SupportsIdempotencyKey.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.communitypaybackapi.controller.internal
+
+import io.swagger.v3.oas.annotations.tags.Tag
+
+@Tag(name = "supportsIdempotencyKey")
+annotation class SupportsIdempotencyKey
